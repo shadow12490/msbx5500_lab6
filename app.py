ece7f94f9428b90a6a7bc0aeac6fea4bb8c481f4
Dave Eargle <dave@daveeargle.com>
2020-04-02 16:08:43 -0600

    add beginnings of psql and mongodb connections

--- app.py
+++ app.py
@@ -1,7 +1,40 @@
-# flask_web/app.py
+from flask import Flask, request, render_template, jsonify
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
+import os
 
-from flask import Flask
 app = Flask(__name__)
+
+# add cross-origin allow to all routes
+CORS(app)
+
+########
+### PSQL
+########
+# get psql db up. quickstart here: https://flask-sqlalchemy.palletsprojects.com/en/2.x/quickstart/
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL')
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self
+
+@app.route('/db_test')
+def get_users():
+    users = User.query.all()
+    return render_template('users.html', users=users)
+
+###########
+### MONGODB
+###########
+# get a mongodb up
+app.config["MONGO_URI"] = os.getenv('MONGODB_URI')
+mongo = PyMongo(app)
 
 @app.route('/')
 def hello_world():
---
+++ init_psql_db.py
@@ -0,0 +1,11 @@
+from app import db, User
+from app import User
+
+db.create_all()
+
+admin = User(username='admin', email='admin@example.com')
+guest = User(username='guest', email='guest@example.com')
+
+db.session.add(admin)
+db.session.add(guest)
+db.session.commit()
