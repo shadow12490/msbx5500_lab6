16781a2ae76cd1ecd87a857e06b16cf0093dc028
Dave Eargle <dave@daveeargle.com>
2020-04-03 17:20:03 -0600

    a toy example of uploading and fetching files using mongodb and javascript and flask and allthethings

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, render_template, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_pymongo import PyMongo
+import gridfs
+
 import os
 
 app = Flask(__name__)
@@ -33,12 +35,40 @@ def get_users():
 ### MONGODB
 ###########
 # get a mongodb up
+# quickstart https://flask-pymongo.readthedocs.io/en/latest/
 app.config["MONGO_URI"] = os.getenv('MONGODB_URI')
 mongo = PyMongo(app)
+fs = gridfs.GridFS(mongo.db) # direct access to the gridfs file system
+                             # within the mongo database
+
+@app.route("/uploads/<path:filename>", methods=["POST"])
+def save_upload(filename):
+    mongo.save_file(filename, request.files["the_file"])
+    return ('', 204) # 204 No Content
+
+def _get_files():
+    return list(fs.find())
+
+@app.route('/files.json')
+def get_files(method=['GET']):
+    files = _get_files()
+    return jsonify([{'filename': file.name} for file in files])
+
+@app.route('/files')
+def show_files(method=['GET']):
+    files = _get_files()
+    # import pdb; pdb.set_trace();
+    return render_template('files.html', files=files)
+
+
+
+
 
 @app.route('/')
 def hello_world():
     return 'Hey, we have Flask in a Docker container!'
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
